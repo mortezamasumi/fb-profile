d5903f8d463448ddd1abef7086f42709aaacfcea
Morteza Masumi <morteza.masumi@gmail.com>
2025-08-18 05:31:19 +0330

    fix: all config using env

--- config/fb-profile.php
+++ config/fb-profile.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'max_avatar_size' => 200,
-    'mobile_required' => false,
-    'email_required' => true,
-    'username_required' => false,
-    'nid_required' => false,
-    'use_passport_number_on_nid' => false,
-    'gender_required' => false,
-    'birth_date_required' => false,
+    'max_avatar_size' => env('PROFILE_AVATAR_SIZE', 200),
+    'mobile_required' => env('PROFILE_MOBILE_REQUIRED', false),
+    'email_required' => env('PROFILE_EMAIL_REQUIRED', false),
+    'username_required' => env('PROFILE_USERNAME_REQUIRED', false),
+    'nid_required' => env('PROFILE_NID_REQUIRED', false),
+    'use_passport_number_on_nid' => env('PROFILE_PASS_NUMBER', false),
+    'gender_required' => env('PROFILE_GENDER_REQUIRED', false),
+    'birth_date_required' => env('PROFILE_BIRTH_DATE_REQUIRED', false),
 ];
